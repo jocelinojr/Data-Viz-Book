3a0ae3f139901e93d90a48a467a0b0ed7306a124
Jocelino Mendes da Silva Júnior <34030698+jocelinojr@users.noreply.github.com>
2019-03-08 14:22:04 -0300

    Add files via upload

--- Data_visualization_book_1.R
+++ Data_visualization_book_1.R
@@ -1,9 +1,37 @@
 install.packages("gapminder")
-
+install.packages(c("broom", "coefplot", "cowplot", "GGally",
+                    "ggrepel", "ggridges", "gridExtra", 
+                    "here", "interplot","margins", "maps", "mapproj", 
+                     "mapdata", "MASS", "quantreg", "rlang", "survey", 
+                     "srvyr", "viridis", "viridisLite"), repos="http://cran.rstudio.com" )
 library(gapminder)
 library(ggplot2)
 library(scales)
+library(here)
 
+install.packages("survey")
+install.packages("coefplot")
+install.packages("GGally")
+install.packages("ggrepel")
+install.packages("ggridges")
+install.packages("gridExtra")
+install.packages("here")
+install.packages("interplot")
+install.packages("margins")
+install.packages("MASS")
+install.packages("quantreg")
+install.packages("rlang")
+install.packages("srvyr")
+install.packages("viridis")
+
+
+devtools::install_github("kjhealy/socviz")
+
+library(gapminder)
+library(dplyr)
+
+
+glimpse(gapminder)
 
 # ploting life expectancy x gdpPercap
 p <- ggplot(data = gapminder, mapping = aes(x=gdpPercap, y=lifeExp))
@@ -61,6 +89,39 @@ gdp + geom_jitter(mapping = aes(colour=year)) + geom_boxplot() +
 
 
 
+
+###################################################
+# PART 2
+
+
+
+gapminder
+
+# Usando Facets para ver como o GDP dos países se comporta com o passar dos anos
+p2 <- ggplot(data=gapminder, mapping=aes(x=year, y=gdpPercap))
+p3 <- ggplot(data=gapminder, mapping=aes(x=year, y=lifeExp))
+# USando facet_warp em vez de facet_grid
+p2 + geom_line(aes(group=country)) + facet_wrap(~continent)
+
+# Improving...
+p2 + geom_line(aes(group=country), colour="gray70") + 
+     geom_smooth(size=1.1, se=FALSE, method = "loess") + 
+     # muda a escala do eixo y, já que as linhas valores estão muito "pegadas"  
+     scale_y_log10(labels=dollar) + 
+     facet_wrap(~continent, ncol = 5) + 
+     labs(title="GDP per capita over time on Five Continents")
+
+p3 + geom_line(aes(group=country), colour="gray70") + 
+  geom_smooth(size=1.1, se=FALSE, method = "loess") + 
+  # muda a escala do eixo y, já que as linhas valores estão muito "pegadas"  
+  facet_wrap(~continent, ncol = 5) + 
+  labs(title="Life Expectancy over time on Five Continents")
+
+
+
+
+
+     
 
 
 
